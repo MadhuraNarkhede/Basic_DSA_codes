56acb8b88a6ad3e101622aa63f057ec386fbf814
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2024-08-04 12:57:00 +0530

    Add files via upload

---
+++ Bit_manipulation_Question3.cpp
@@ -0,0 +1,26 @@
+//Write a program to generat eall possible subset of set.
+#include<iostream>
+using namespace std;
+int main()
+{   int n;
+    cout<<"Enter the size of the set: ";
+    cin>>n;
+    int set[n];
+    cout<<"Enter the elements of the set: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>set[i];
+    }
+    int total_subset=1<<n;
+    for(int i=0;i<total_subset;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i&(1<<j))
+            {
+                cout<<set[j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
