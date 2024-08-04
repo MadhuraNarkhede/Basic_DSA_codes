07ee23dacff9637efb964f37759cef509bf8bc2e
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2024-08-04 13:50:27 +0530

    Add files via upload

---
+++ Bit_manipulation_Question4.cpp
@@ -0,0 +1,24 @@
+//Writ ethe program to find the unique number in an array where every element appears twice except one.
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int unique(int arr[],int n)
+{
+     int xorsum=0;
+
+     for(int i=0;i<n;i++)
+     {
+        xorsum=xorsum^arr[i];
+     }
+
+     return xorsum;
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,3,2,1};
+    cout<<unique(arr,7);
+
+    return 0;
+}
