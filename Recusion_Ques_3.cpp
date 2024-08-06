83b02f07064356af3615f714d2b5e2bfc6e9e1f4
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2024-08-06 22:54:08 +0530

    Add files via upload

---
+++ Recusion_Ques_3.cpp
@@ -0,0 +1,23 @@
+//Write the code to find factorial of a number.
+
+#include <iostream> 
+using namespace std;
+
+int factorial(int n)
+{
+   if(n==0)
+   {
+      return 1;
+   }
+
+   int prevfact=factorial(n-1);
+   return n*prevfact;
+}
+int main()
+{
+   int n;
+   cin>>n;
+
+   cout<<factorial(n)<<endl;
+}
+
