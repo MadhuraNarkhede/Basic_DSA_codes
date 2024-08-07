38e482bd06b67cc9f55163df24543df8e7def2ea
Madhura Lahu  Narkhede <156207107+MadhuraNarkhede@users.noreply.github.com>
2024-08-07 13:58:18 +0530

    Add files via upload

---
+++ Recusion_Ques_10.cpp
@@ -0,0 +1,31 @@
+//Write the code to remove duplicate of a string using recursion.
+
+#include <iostream>
+using namespace std;
+
+string removeDup(string s)
+{
+  if(s.length()==0)
+  {
+    return " ";
+
+  }
+
+char ch=s[0];
+string ans=removeDup(s.substr(1));
+
+if(ch==ans[0])
+{
+  return ans;
+}
+
+return ch+ans;
+}
+
+int main()
+{ 
+
+  cout<<removeDup("aaaabbbeeecdddd")<<endl;
+}
+  
+  
